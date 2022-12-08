e317f19f62a814e9dd477a6a5bea6dada3fd14cc
aris <arisvomv@gmail.com>
2022-12-08 13:10:00 +0200

    basic swiper testing

--- js/global.js
+++ js/global.js
@@ -1,71 +1,76 @@
-let screenWidth = window.innerWidth;
-
-let target = {};
-let proxy = new Proxy(target, {}); // empty handler
-
-console.log(proxy);
-proxy.test = 5; // writing to proxy (1)
-console.log(proxy); // 5, the property appeared in target!
-
-console.log(proxy); // 5, we can read it from proxy too (2)
-
-for (let key in proxy) console.log(key);
-
 document.addEventListener('DOMContentLoaded', () => {
   // Global
   const burger = document.querySelector('.js-burger');
   const menu = document.querySelector('.js-menu');
-
+  let screenWidth = window.innerWidth;
   // Services
-  let serviceCards = document.querySelector('.js-service-cards');
+  let verticalTeasers = {
+    el: document.querySelector('.js-vertical-teasers'),
+    slider: null,
+  };
+  let valueTeasers = {
+    el: document.querySelector('.js-values'),
+    slider: null,
+  };
 
   burger.addEventListener('click', () => menu.classList.toggle('active'));
 
-  if (serviceCards && screenWidth <= 360) {
-    service_cards.create();
-  }
+  if (verticalTeasers.el) check_vertical_teasers();
+  if (valueTeasers.el) check_value_teasers();
 
   window.addEventListener('resize', () => {
     screenWidth = window.innerWidth;
+    if (verticalTeasers.el) check_vertical_teasers();
+    if (valueTeasers.el) check_value_teasers();
   });
 
-  const service_cards = {
-    create: () => {},
-    destroy: () => {},
-  };
+  function check_vertical_teasers() {
+    if (screenWidth <= 360) {
+      if (!!!verticalTeasers.slider || verticalTeasers.slider?.destroyed) carousel_vertical_teasers();
+    } else {
+      if (verticalTeasers.slider?.enabled) verticalTeasers.slider.destroy(true, true);
+    }
+  }
+
+  function carousel_vertical_teasers() {
+    console.log('carousel_vertical_teasers');
+    verticalTeasers.slider = new Swiper('.js-vertical-teasers', {
+      init: true,
+      wrapperClass: 'split',
+      slideClass: 'vertical-teaser',
+      slideActiveClass: 'active',
+      pagination: {
+        el: '.pagination',
+        type: 'bullets',
+        bulletClass: 'pagination__bullet',
+        bulletActiveClass: 'active',
+        clickable: true,
+      },
+    });
+  }
+
+  function carousel_values() {
+    console.log('carousel_values');
+    valueTeasers.slider = new Swiper('.js-values', {
+      init: true,
+      wrapperClass: 'split',
+      slideClass: 'split__part',
+      slideActiveClass: 'active',
+      pagination: {
+        el: '.pagination',
+        type: 'bullets',
+        bulletClass: 'pagination__bullet',
+        bulletActiveClass: 'active',
+        clickable: true,
+      },
+    });
+  }
+
+  function check_value_teasers() {
+    if (screenWidth <= 360) {
+      if (!!!valueTeasers.slider || valueTeasers.slider?.destroyed) carousel_values();
+    } else {
+      if (valueTeasers.slider?.enabled) valueTeasers.slider.destroy(true, true);
+    }
+  }
 });
-
-// let homeCards = document.querySelector('.js-service-cards');
-
-// homeCards = new Swiper('.cards', {
-//   init: false,
-//   wrapperClass: 'cards__wrap',
-//   slideClass: 'card',
-//   slideActiveClass: 'active',
-//   pagination: {
-//     el: '.cards__pagination',
-//     type: 'bullets',
-//     bulletClass: 'card__bullet',
-//     bulletActiveClass: 'active',
-//     clickable: true,
-//   },
-// });
-
-// if (serviceCards.classList.contains('cards--default')) {
-//   // serviceCards.classList.remove('cards--default');
-//   // serviceCardsSlider = new Swiper('.cards', {
-//   //   init: true,
-//   //   wrapperClass: 'cards__wrap',
-//   //   slideClass: 'card',
-//   //   slideActiveClass: 'active',
-//   //   pagination: {
-//   //     el: '.cards__pagination',
-//   //     type: 'bullets',
-//   //     bulletClass: 'card__bullet',
-//   //     bulletActiveClass: 'active',
-//   //     clickable: true,
-//   //   },
-//   // });
-
-//   // console.log(serviceCards?.$el?.[0]);
-// }
