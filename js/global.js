df1b985e862e4dc02d36317cdce1bfa7f492e996
aris <arisvomv@gmail.com>
2022-12-10 09:47:11 +0200

    personas slider

--- js/global.js
+++ js/global.js
@@ -3,13 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector('.js-burger');
   const menu = document.querySelector('.js-menu');
   let screenWidth = window.innerWidth;
-  // Services
   let verticalTeasers = {
     el: document.querySelector('.js-vertical-teasers'),
     slider: null,
   };
   let valueTeasers = {
     el: document.querySelector('.js-values'),
+    slider: null,
+  };
+  let personas = {
+    el: document.querySelector('.js-personas'),
     slider: null,
   };
 
@@ -20,12 +23,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (verticalTeasers.el) check_vertical_teasers();
   if (valueTeasers.el) check_value_teasers();
+  if (personas.el) check_personas();
 
   window.addEventListener('resize', () => {
     screenWidth = window.innerWidth;
     if (verticalTeasers.el) check_vertical_teasers();
     if (valueTeasers.el) check_value_teasers();
+    if (personas.el) check_personas();
   });
+
+  function check_personas() {
+    if (screenWidth <= 528) {
+      if (!!!personas.slider || personas.slider?.destroyed) carousel_personas();
+    } else {
+      if (personas.slider?.enabled) personas.slider.destroy(true, true);
+    }
+  }
+  function carousel_personas() {
+    console.log('carousel_personas');
+    personas.slider = new Swiper('.js-personas', {
+      slidesPerView: 'auto',
+      // spaceBetween: 24,
+      centeredSlides: true,
+      // centeredSlidesBounds: true,
+      // loopAdditionalSlides: 2,
+      // loop: true,
+      // rewind: true,
+      // pagination: {
+      //   el: ".swiper-pagination",
+      //   clickable: true,
+      // },
+
+      // init: true,
+      // autoHeight: true,
+      wrapperClass: 'split',
+      slideClass: 'split__part',
+      slideActiveClass: 'active',
+      pagination: {
+        el: '.pagination',
+        type: 'bullets',
+        bulletClass: 'pagination__bullet',
+        bulletActiveClass: 'active',
+        clickable: true,
+      },
+    });
+  }
 
   function check_vertical_teasers() {
     if (screenWidth <= 360) {
