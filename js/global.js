25554fd21ee50148a8353f39c7dc5d03711dbe6f
aris <arisvomv@gmail.com>
2023-02-12 02:49:51 +0200

    careers n personas in external files

--- js/global.js
+++ js/global.js
@@ -1,3 +1,6 @@
+import { personas } from './personas.js';
+import { careers } from './careers.js';
+
 document.addEventListener('DOMContentLoaded', () => {
   const burger = document.querySelector('.js-burger');
   const menu = document.querySelector('.js-menu');
@@ -10,10 +13,10 @@ document.addEventListener('DOMContentLoaded', () => {
     menu.classList.toggle('active');
   });
 
-  if (document.querySelector('.js-personas')) new Personas('.js-personas');
+  if (document.getElementById('personas')) handle_personas('personas');
+  if (document.getElementById('careers')) handle_careers('careers');
   if (document.querySelector('.js-horizontal-teasers')) new HorizontalTeasers('.js-horizontal-teasers');
   if (document.querySelector('.js-values')) new Values('.js-values');
-  if (document.querySelector('.js-accordion')) new Accordion('.js-accordion');
   if (document.querySelectorAll('[data-clock]').length) new Clocks('[data-clock]');
   if (document.querySelectorAll('[data-location]').length) new Locations('[data-location]');
 });
@@ -36,76 +39,6 @@ class Locations {
         [...this.popups].forEach(el => el.classList.remove('active'));
       });
     });
-  }
-}
-
-class Personas {
-  constructor(selector) {
-    this.selector = selector;
-    this.el = document.querySelector(selector);
-    this.personas = this.el.querySelectorAll('.persona');
-    this.carousel = null;
-    this.parent = this.el.closest('.personas__wrap');
-    this.breakPoint = 528;
-    this.body = document.querySelector('body');
-    this.init();
-  }
-  init() {
-    this.watch_carousel();
-
-    window.addEventListener('resize', () => {
-      this.watch_carousel();
-      this.personas.forEach(persona => persona.classList.remove('open'));
-      this.handle_global_scroll();
-    });
-
-    this.personas.forEach(persona => {
-      persona.addEventListener('click', () => {
-        this.personas.forEach(el => {
-          if (el.isSameNode(persona)) {
-            el.classList.toggle('open');
-          } else {
-            el.classList.remove('open');
-          }
-        });
-
-        this.handle_global_scroll();
-      });
-
-      persona.querySelector('.persona__bio').addEventListener('click', e => e.stopPropagation());
-      persona.querySelector('.persona__close').addEventListener('click', () => {
-        persona.classList.remove('open');
-        this.handle_global_scroll();
-      });
-    });
-  }
-  watch_carousel() {
-    if (window.innerWidth <= this.breakPoint) {
-      if (!!!this.carousel || this.carousel?.destroyed) this.create_carousel();
-    } else {
-      if (this.carousel?.enabled) this.carousel.destroy(true, true);
-    }
-  }
-  create_carousel() {
-    this.carousel = new Swiper(this.selector, {
-      slidesPerView: 'auto',
-      centeredSlides: true,
-      wrapperClass: 'split',
-      slideClass: 'split__part',
-      slideActiveClass: 'active',
-      mousewheel: {
-        forceToAxis: true,
-      },
-    });
-  }
-  handle_global_scroll() {
-    if (window.innerWidth >= this.breakPoint) {
-      if ([...this.personas].filter(persona => persona.classList.contains('open')).length) {
-        this.body.classList.add('no-scroll');
-      } else {
-        this.body.classList.remove('no-scroll');
-      }
-    }
   }
 }
 class HorizontalTeasers {
@@ -212,79 +145,208 @@ class Clocks {
     });
   }
 }
-class Accordion {
-  constructor(selector) {
-    this.selector = selector;
-    this.accordion = document.querySelector(this.selector);
-    this.items = this.accordion.querySelectorAll('.accordion__item');
-    this.activePosition = null;
-    this.body = document.querySelector('body');
-    this.init();
-  }
-  init() {
-    this.items.forEach((item, index) => {
-      item.addEventListener('click', () => {
-        if (index == this.activePosition) {
-          this.close_item(item);
-          this.activePosition = null;
-        } else {
-          if (this.activePosition == null) {
-            this.activePosition = index;
-            this.open_item(item);
-          } else {
-            this.close_item(this.items[this.activePosition]);
-            this.activePosition = index;
-            this.open_item(item);
-          }
-        }
-      });
 
-      item.querySelectorAll('[data-trigger]').forEach(trigger => {
-        trigger.addEventListener('click', e => {
-          e.stopPropagation();
-          let triggerPosition = trigger.getAttribute('data-trigger');
-          item.querySelector(`[data-position="${triggerPosition}"]`).classList.add('open');
-          this.handle_global_scroll();
-        });
-      });
-
-      item.querySelectorAll('[data-position]').forEach(position => {
-        position.addEventListener('click', e => {
-          e.stopPropagation();
-          if (e.target.isSameNode(position)) position.classList.remove('open');
-          this.handle_global_scroll();
-        });
-        position.querySelector('.position__close').addEventListener('click', () => position.classList.remove('open'));
-      });
-    });
-
-    window.addEventListener('resize', () => {
-      if (this.activePosition != null) {
-        this.activePosition = null;
-        this.items.forEach(item => {
-          item.classList.remove('active');
-          item.querySelector('.accordion__body').style.removeProperty('height');
-          item.querySelectorAll('[data-position]').forEach(position => position.classList.remove('open'));
-        });
-        this.handle_global_scroll();
-      }
-    });
-  }
-  handle_global_scroll() {
-    let openPopups = [...this.items].filter(item => {
-      let popup = item.querySelectorAll('[data-position].open');
-      if (popup.length) return popup;
-    });
-    openPopups.length ? this.body.classList.add('no-scroll') : this.body.classList.remove('no-scroll');
-  }
-  open_item(item) {
-    item.classList.add('active');
-    let itemBody = item.querySelector('.accordion__body');
-    let height = itemBody.querySelector('.accordion__content').scrollHeight;
-    itemBody.style.setProperty('height', `${height}px`);
-  }
-  close_item(item) {
-    item.classList.remove('active');
-    item.querySelector('.accordion__body').style.removeProperty('height');
-  }
+function handle_personas(selector) {
+  new Vue({
+    el: `#${selector}`,
+    data: () => ({ personas }),
+    methods: {
+      open_bio(index) {
+        let currentState = this.personas[index].isOpen;
+        this.personas.forEach(persona => (persona.isOpen = false));
+        this.personas[index].isOpen = !currentState;
+      },
+      close_bio(index) {
+        this.personas[index].isOpen = false;
+      },
+    },
+  });
 }
+
+function handle_careers(selector) {
+  new Vue({
+    el: `#${selector}`,
+    data: () => ({
+      careers,
+      activeCategory: null,
+      activePosition: null,
+    }),
+    mounted() {
+      window.addEventListener('resize', () => {
+        this.activeCategory = null;
+        this.activePosition = null;
+      });
+    },
+    methods: {
+      toggle_category(index) {
+        this.activeCategory = this.activeCategory == index ? null : index;
+      },
+      open_position(index) {
+        this.activePosition = index;
+      },
+      close_position() {
+        this.activePosition = null;
+      },
+    },
+    watch: {
+      activeCategory(newVal) {
+        console.log(newVal);
+        for (let item in this.$refs) {
+          this.$refs[item][0].querySelector('.accordion__body').style.removeProperty('height');
+        }
+        if (newVal != null) {
+          let itemBody = this.$refs[`category-${newVal}`][0].querySelector('.accordion__body');
+          let height = itemBody.querySelector('.accordion__content').scrollHeight;
+          itemBody.style.setProperty('height', `${height}px`);
+        }
+      },
+    },
+  });
+}
+
+// class Accordion {
+//   constructor(selector) {
+//     this.selector = selector;
+//     this.accordion = document.querySelector(this.selector);
+//     this.items = this.accordion.querySelectorAll('.accordion__item');
+//     this.activePosition = null;
+//     this.body = document.querySelector('body');
+//     this.init();
+//   }
+//   init() {
+//     this.items.forEach((item, index) => {
+//       item.addEventListener('click', () => {
+//         if (index == this.activePosition) {
+//           this.close_item(item);
+//           this.activePosition = null;
+//         } else {
+//           if (this.activePosition == null) {
+//             this.activePosition = index;
+//             this.open_item(item);
+//           } else {
+//             this.close_item(this.items[this.activePosition]);
+//             this.activePosition = index;
+//             this.open_item(item);
+//           }
+//         }
+//       });
+
+//       item.querySelectorAll('[data-trigger]').forEach(trigger => {
+//         trigger.addEventListener('click', e => {
+//           e.stopPropagation();
+//           let triggerPosition = trigger.getAttribute('data-trigger');
+//           item.querySelector(`[data-position="${triggerPosition}"]`).classList.add('open');
+//           this.handle_global_scroll();
+//         });
+//       });
+
+//       item.querySelectorAll('[data-position]').forEach(position => {
+//         position.addEventListener('click', e => {
+//           e.stopPropagation();
+//           if (e.target.isSameNode(position)) position.classList.remove('open');
+//           this.handle_global_scroll();
+//         });
+//         position.querySelector('.position__close').addEventListener('click', () => position.classList.remove('open'));
+//       });
+//     });
+
+//     window.addEventListener('resize', () => {
+//       if (this.activePosition != null) {
+//         this.activePosition = null;
+//         this.items.forEach(item => {
+//           item.classList.remove('active');
+//           item.querySelector('.accordion__body').style.removeProperty('height');
+//           item.querySelectorAll('[data-position]').forEach(position => position.classList.remove('open'));
+//         });
+//         this.handle_global_scroll();
+//       }
+//     });
+//   }
+//   handle_global_scroll() {
+//     let openPopups = [...this.items].filter(item => {
+//       let popup = item.querySelectorAll('[data-position].open');
+//       if (popup.length) return popup;
+//     });
+//     openPopups.length ? this.body.classList.add('no-scroll') : this.body.classList.remove('no-scroll');
+//   }
+//   open_item(item) {
+//     item.classList.add('active');
+//     let itemBody = item.querySelector('.accordion__body');
+//     let height = itemBody.querySelector('.accordion__content').scrollHeight;
+//     itemBody.style.setProperty('height', `${height}px`);
+//   }
+//   close_item(item) {
+//     item.classList.remove('active');
+//     item.querySelector('.accordion__body').style.removeProperty('height');
+//   }
+// }
+
+// class Personas {
+//   constructor(selector) {
+//     this.selector = selector;
+//     this.el = document.querySelector(selector);
+//     this.personas = this.el.querySelectorAll('.persona');
+//     this.carousel = null;
+//     this.parent = this.el.closest('.personas__wrap');
+//     this.breakPoint = 528;
+//     this.body = document.querySelector('body');
+//     this.init();
+//   }
+//   init() {
+//     this.watch_carousel();
+
+//     window.addEventListener('resize', () => {
+//       this.watch_carousel();
+//       this.personas.forEach(persona => persona.classList.remove('open'));
+//       this.handle_global_scroll();
+//     });
+
+//     this.personas.forEach(persona => {
+//       persona.addEventListener('click', () => {
+//         this.personas.forEach(el => {
+//           if (el.isSameNode(persona)) {
+//             el.classList.toggle('open');
+//           } else {
+//             el.classList.remove('open');
+//           }
+//         });
+
+//         this.handle_global_scroll();
+//       });
+
+//       persona.querySelector('.persona__bio').addEventListener('click', e => e.stopPropagation());
+//       persona.querySelector('.persona__close').addEventListener('click', () => {
+//         persona.classList.remove('open');
+//         this.handle_global_scroll();
+//       });
+//     });
+//   }
+//   watch_carousel() {
+//     if (window.innerWidth <= this.breakPoint) {
+//       if (!!!this.carousel || this.carousel?.destroyed) this.create_carousel();
+//     } else {
+//       if (this.carousel?.enabled) this.carousel.destroy(true, true);
+//     }
+//   }
+//   create_carousel() {
+//     this.carousel = new Swiper(this.selector, {
+//       slidesPerView: 'auto',
+//       centeredSlides: true,
+//       wrapperClass: 'split',
+//       slideClass: 'split__part',
+//       slideActiveClass: 'active',
+//       mousewheel: {
+//         forceToAxis: true,
+//       },
+//     });
+//   }
+//   handle_global_scroll() {
+//     if (window.innerWidth >= this.breakPoint) {
+//       if ([...this.personas].filter(persona => persona.classList.contains('open')).length) {
+//         this.body.classList.add('no-scroll');
+//       } else {
+//         this.body.classList.remove('no-scroll');
+//       }
+//     }
+//   }
+// }
