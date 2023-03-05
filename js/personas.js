00632b0ed9dec890b4c7483983a46886aad0955d
aris <arisvomv@gmail.com>
2023-03-05 20:59:24 +0200

    dummy personas

--- js/personas.js
+++ js/personas.js
@@ -336,4 +336,342 @@ export const personas = [
       </p>
     `,
   },
+
+  {
+    id: 9,
+    name: 'Vangelis Vergetis, PhD',
+    title: 'CEO and board member',
+    filters: [
+      { position: 'all', order: 8 },
+      { position: 'board', order: 5 },
+      { position: 'executive', order: 1 },
+    ],
+    img: 'assets/images/Vangelis_vergetis.png',
+    linkedin: 'https://www.linkedin.com/in/vangelisvergetis',
+    bio: `
+      <p>
+        Vangelis has been working in the intersection 
+        of healthcare, technology, and data science 
+        for more than 15 years.
+      </p>
+      <p>
+        He started his career as a consultant at McKinsey and at Hakluyt 
+        for about a douzen years, advising biopharmaceutical executives 
+        on several topics - particularly in launching drugs across 
+        disease areas, and in designing and putting in 
+        place their commercial models and infrastructure. 
+      </p>
+      <p>
+        
+      </p>
+      <p>
+        Immediately prior to Epikast, Vangelis was the 
+        co-founder and CEO of Intelligencia, a software 
+        company that utilizes AI to assess and reduce 
+        the risk of clinical trials. He currently serves 
+        as a board advisor to the company.
+      </p>
+      <p>
+        He is a board member and member of the executive committee 
+        at the Alliance for AI in Healthcare (AAIH), 
+        and a former member of the advisory board for 
+        Cornell's Bowers College of Computing &amp; 
+        Information Science (CIS).
+      </p>
+      <p>
+        Vangelis holds a 
+        PhD in Electrical Engineering from the University 
+        of Pennsylvania, and a BS in 
+        Computer Science from Cornell.
+      </p>
+    `,
+  },
+  {
+    id: 10,
+    name: 'Stelios Papadopoulos, PhD',
+    title: 'Chairman of the board',
+    filters: [
+      { position: 'all', order: 5 },
+      { position: 'board', order: 1 },
+    ],
+    img: 'assets/images/Stelios_papadopoulos.jpg',
+    linkedin: null,
+    bio: `
+      <p>
+        Stelios brings more than four decades of experience in the 
+        biopharmaceutical industry in various roles: as a company founder, 
+        executive, investment banker, and leader in the global healthcare 
+        sector.
+      </p>
+      <p>
+        Stelios is Chairman of the Board of Directors of Biogen, Inc., 
+        Exelixis, Inc., and Regulus Therapeutics, Inc. He is a co-founder 
+        of Exelixis, as well as co-founder and former Chairman of Anadys 
+        Pharmaceuticals (acquired by Hoffman–La Roche in 2011) and 
+        Cellzome (acquired by GlaxoSmithKline in 2012). In 
+        the not-for-profit sector, Stelios is a member of the 
+        Board of Visitors of Duke Medicine, a member of the Global 
+        Advisory Board of the Duke Institute for Health Innovation, 
+        and co-founder and Chairman of Fondation Santé 
+        (<a href="https://www.fondationsante.org">www.fondationsante.org</a>), a foundation providing 
+        research support and mentorship to biomedical 
+        scientists in Greece and Cyprus.
+      </p>
+      <p>
+        Stelios spent six years (2000-06) at Cowen & Co., LLC, most 
+        recently as Vice Chairman, where as an investment banker he 
+        focused on the biotech and pharma sectors. Prior to joining 
+        Cowen, he spent 13 years as an investment banker at PaineWebber, 
+        Incorporated where he was most recently Chairman of 
+        PaineWebber Development Corp., a PaineWebber subsidiary 
+        focusing on biotechnology. He joined PaineWebber in 1987 
+        from Drexel Burnham Lambert where he was an analyst in 
+        the Equity Research Department covering the biotechnology industry. 
+        Prior to Drexel, he was the biotechnology analyst of Donaldson, 
+        Lufkin & Jenrette. Stelios has received multiple honors and 
+        awards for his work in the biopharma industry as a company 
+        founder, adviser, and financier.
+      </p>
+      <p>
+        Before coming to Wall Street, Stelios was on the faculty of the 
+        Department of Cell Biology at New York University School of 
+        Medicine. Stelios holds an M.S. in physics, a Ph.D. in biophysics 
+        and an M.B.A. in finance, all from New York University.
+      </p>
+    `,
+  },
+  {
+    id: 11,
+    name: 'Daphne Karydas',
+    title: 'Board member',
+    filters: [
+      { position: 'all', order: 2 },
+      { position: 'board', order: 2 },
+    ],
+    img: 'assets/images/Daphne.jpg',
+    linkedin: 'https://www.linkedin.com/in/daphne-karydas-493b17/',
+    bio: `
+      <p>
+        Daphne has over 25 years of experience in 
+        financial and operations leadership roles 
+        focused in the biopharmaceutical industry, 
+        where she has brought an interdisciplinary 
+        approach to implementing growth strategies 
+        for biopharmaceutical and asset 
+        management companies.
+      </p>
+      <p>
+        She is currently the COO and CFO of Flare 
+        Therapeutics, which she joined in October 2021. 
+        Prior to Flare she was the CFO of Syndax 
+        Pharmaceuticals, and also served as SVP of 
+        Strategy and Corporate Financial Planning & 
+        Analysis at Allergan plc, where she oversaw 
+        the company’s long-term financial and 
+        business strategy until its acquisition 
+        by Abbvie in May 2020. She joined 
+        Allergan in April 2017 as the SVP of 
+        Global Investor Relations and Strategy, 
+        leading engagement with the investment 
+        community and business strategy development.
+      </p>
+      <p>
+        Prior to her senior operating roles in the industry, 
+        Daphne served as Executive Director and Senior 
+        Healthcare Analyst at J.P. Morgan Asset Management. 
+        Previously, she was a Portfolio Manager and Senior 
+        Healthcare Analyst at The Boston Company Asset 
+        Management, a BNY Mellon company. Earlier in 
+        her career she was a VP at Goldman Sachs Asset 
+        Management focused on healthcare, as well as a 
+        member of Goldman Sachs’ healthcare investment 
+        banking team. Before joining Goldman Sachs she 
+        was a Project Chemical Engineer at Merck, 
+        where she focused on process development 
+        for novel vaccines.
+      </p>
+      <p>
+        Daphne received a BA and MS in chemical engineering 
+        from MIT and an MBA from Harvard Business School.
+      </p>
+    `,
+  },
+  {
+    id: 12,
+    name: 'Dimitris Papalexopoulos',
+    title: 'Board member',
+    filters: [
+      { position: 'all', order: 6 },
+      { position: 'board', order: 4 },
+    ],
+    img: 'assets/images/Dimitris_Papalexopoulos.jpg',
+    linkedin: null,
+    bio: `
+      <p>
+        Dimitri is Chairman of Titan Cement Group, a cement 
+        and building materials producer active since 1902. 
+        The group directly employs about 5,500 people, 
+        operates cement plants in 10 countries, and is 
+        active in many more. Prior to joining Titan in 1989, 
+        Dimirti started his career as a business consultant 
+        for McKinsey & Company in the USA and Germany.
+      </p>
+      <p>
+        He is Chairman of the Board of the Hellenic Federation 
+        of Enterprises (SEV), Vice-Chairman of the European 
+        Round Table for Industry (ERT) and chairs the ERT’s 
+        Energy Transition & Climate Change Committee. 
+        He is a member of the Board of the Foundation for 
+        Economic and Industrial Research (IOBE), the 
+        Hellenic Foundation for European and Foreign Policy 
+        (ELIAMEP), and of Endeavor Greece.
+      </p>
+      <p>
+        He holds a M.Sc. 
+        in Electrical Engineering from the Swiss Federal 
+        Institute of Technology (ETHZ) and an MBA 
+        from Harvard Business School.
+      </p>
+    `,
+  },
+  {
+    id: 13,
+    name: 'Mike Krzan',
+    title: 'Advisor',
+    filters: [
+      { position: 'all', order: 3 },
+      { position: 'advisors', order: 1 },
+    ],
+    img: 'assets/images/Mike_Krzan.jpg',
+    linkedin: 'https://www.linkedin.com/in/mike-krzan',
+    bio: `
+      <p>
+        Mike brings a lifetime of experience in building and 
+        running high-performing patient services organizations, 
+        having spent more than 20 years in the space. He led 
+        Biogen’s patient services center in North Carolina 
+        for more than a decade, and prior to that was a SVP 
+        for member services and pharmacy operations at Medco. 
+      </p>
+      <p>
+        While at Biogen, Mike was a member of the US Senior 
+        Leadership Team, developed and set the direction for 
+        the patient services organization to support $8B of 
+        revenue, launched seven products (including a blockbuster 
+        drug and an orphan drug), and collaborated with 
+        field sales leaders to identify trends, define 
+        tactics for improved performance, and ensure national 
+        team alignment. 
+      </p>
+      <p>
+        He holds a BS in Pharmacy 
+        from Duquesne University.
+      </p>
+    `,
+  },
+  {
+    id: 14,
+    name: 'Angelos Kapsimanis',
+    title: 'Chief Technology Officer',
+    filters: [
+      { position: 'all', order: 1 },
+      { position: 'executive', order: 2 },
+    ],
+    img: 'assets/images/Angelos.png',
+    linkedin: 'https://www.linkedin.com/in/angeloskapsimanis',
+    bio: `
+      <p>
+        Angelos has been working in the tech industry for more than two decades, 
+        with experience across 65+ countries. He has led teams across multiple 
+        verticals and industries, with a focus on data, machine learning, and AI.
+      </p>
+      <p>
+        He has spent a significant amount of his career in the FinTech industry, 
+        helping Klarna to expand globally and reach a valuation of $70B. 
+        While at Klarna he managed teams interspersed across the planet, 
+        focusing on analytics, machine learning, real-time decision making, 
+        and risk management. Prior to Klarna, Angelos built the whole technology 
+        function for Motionlogic, an analytics company that is part of the Deutsche 
+        Telekom group, and helped it achieve a 52-fold increase in revenues in 3 years. 
+        He bootstrapped and successful led to exit Aton Solar, which developed embedded 
+        systems and telemetry solution for solar energy facilities.
+      </p>
+      <p>
+        He holds an MS in Machine Learning from Georgia Tech, a second Masters 
+        in Computational Statistics from De Montfort University in the UK, and a 
+        BS in Industrial Informatics in the University of Applied Sciences in Kavala, Greece.
+      </p>
+    `,
+  },
+  {
+    id: 15,
+    name: 'Oleg Nodelman',
+    title: 'Board member',
+    filters: [
+      { position: 'all', order: 4 },
+      { position: 'board', order: 3 },
+    ],
+    img: 'assets/images/Oleg.png',
+    linkedin: 'https://www.linkedin.com/in/oleg-nodelman-375131',
+    bio: `
+      <p>
+        Oleg is the founder and Portfolio Manager of EcoR1 Capital LLC, 
+        a biotech-focused investment fund established in 2013, 
+        which invests in companies at all stages of research and 
+        development. With nearly twenty years of experience in 
+        biotech investing, he has expertise in all aspects of 
+        investment management and deep roots in the biotech 
+        and scientific communities. 
+      </p>
+      <p>
+        Before founding EcoR1, Oleg was a portfolio manager at BVF 
+        Partners, one of the first hedge funds dedicated to the 
+        biotechnology sector. He currently serves as a Board 
+        Member for Prothena Biosciences (NASDAQ: PRTA), AnaptysBio 
+        (Nasdaq: ANAB), Nuvation Bio (Nasdaq: NUVB), Panacea 
+        (Nasdaq: PANA), Scorpion Therapeutics, Aktis Oncology, 
+        ReCode, and Paratus. 
+      </p>
+      <p>
+        Oleg holds a BS in Foreign Service with a concentration in 
+        Science and Technology from Georgetown University.
+      </p>
+    `,
+  },
+  {
+    id: 16,
+    name: 'Amit Singhal, PhD',
+    title: 'Advisor',
+    filters: [
+      { position: 'all', order: 7 },
+      { position: 'advisors', order: 2 },
+    ],
+    img: 'assets/images/Amit.jpg',
+    linkedin: 'https://www.linkedin.com/in/amit-singhal-09723574/',
+    bio: `
+      <p>
+        Amit is known as the man behind Google Search, having led the 
+        Google Search team for over 15 years - from its early days 
+        in 2000 until his departure in 2016. He has received several 
+        awards and distinctions, including being named as a Google Fellow, 
+        elected to the National Academy of Engineering, elected Fellow 
+        of the ACM (Association for Computing Machinery), and 
+        called one of the "smartest people in tech" by Fortune magazine.
+      </p>
+      <p>
+        He is currently dedicated to giving back to his community 
+        through the Sitare Foundation (<a href="https://www.sitare.org">www.sitare.org</a>), a non-profit 
+        organization that he co-founded together with his wife, Shilpa. 
+        Sitare is focusing on providing access to high quality education 
+        to the most talented underprivileged children in India. Amit is 
+        also an active angel investor and an advisor to 
+        technology companies globally.
+      </p>
+      <p>
+        He holds a Bachelor of Engineering degree from IIT Roorkee, 
+        an MS from the University of Minnesota Duluth, and a PhD from 
+        Cornell University, all in Computer Science and Information Retrieval.
+      </p>
+    `,
+  },
 ];
