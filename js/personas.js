84dbaeb834998fc68a8c6ba93e68c8045113313d
Vangelis Vergetis <121926953+vergetis@users.noreply.github.com>
2023-03-06 17:15:05 -0500

    Update personas.js

--- js/personas.js
+++ js/personas.js
@@ -17,11 +17,11 @@ export const personas = [
         for more than 15 years.
       </p>
       <p>
-        He started his career as a consultant at McKinsey and at Hakluyt 
-        for about a dozen years, advising biopharmaceutical executives 
+        He started his career working for twelve years as a consultant 
+        at McKinsey and at Hakluyt, where he advised biopharmaceutical executives 
         on several topics - particularly in launching drugs across 
-        disease areas, and in designing and putting in 
-        place their commercial models and infrastructure. 
+        disease areas, and in designing and implementing
+        their commercial models and infrastructure. 
       </p>
       <p>
         
