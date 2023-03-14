f58829d45d5d43f6643676838254a66749c15bdc
Vangelis Vergetis <121926953+vergetis@users.noreply.github.com>
2023-03-14 09:46:01 -0400

    Update personas.js

--- js/personas.js
+++ js/personas.js
@@ -342,7 +342,7 @@ export const personas = [
     title: 'Advisor',
     filters: [
       { position: 'all', order: 9 },
-      { position: 'board', order: 5 },
+      { position: 'advisors', order: 3 },
     ],
     img: 'assets/images/Nassos_Zarkalis.jpg',
     linkedin: null,
