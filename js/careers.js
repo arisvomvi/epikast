bddb19af4ee3e7576d7b0383f11cb5076f9ec1ef
Angelos Kapsimanis <angelos@clearhq.ai>
2023-06-05 23:18:41 +0200

    Update job posts

--- js/careers.js
+++ js/careers.js
@@ -3,6 +3,219 @@ export const careers = new Map([
     { category: 'Patient & Physician Services ', icon: 'assets/customer_service.svg', isOpen: false },
     [
       {
+        title: 'Trainer Supervisor (Patient & Physician services)',
+        location: 'Athens, Greece',
+        description: `
+          <p>
+            At Epikast we redefine how biopharmaceutical companies engage 
+            with their most important stakeholders: patients, caregivers 
+            and prescribers.
+          </p>
+          <p>
+            As a Training Supervisor you will be responsible to manage 
+            the initiation, development and implementation of the training 
+            departments both in class room and remote training. You will be 
+            required to assess current training and development systems, 
+            enhance and develop new training programs to enhance productivity 
+            and quality . You will also be required to conduct research to 
+            identify specific staff training needs. The role is accountable 
+            also to develop digital training material in cooperation with 
+            instructional designer and developers of the team.
+          </p>
+          <p>
+            The Training Supervisor should help patient engagement contact 
+            centre front-liners to acquire knowledge, skills and competencies
+            in an effective and appealing manner.You should be proficient in 
+            training methodologies and demonstrate superior teamwork abilities.
+          </p>
+
+          <h4>Key accountabilities and decision ownership: </h4>
+          <ul>
+            <li>Designing and administering training programs</li>
+            <li>Providing expertise in development design</li>
+            <li>
+              Identifying training needs and providing recommendations 
+              on programs and activities to address learning, 
+              development and cultural areas
+            </li>
+            <li>
+              Participates in the development of annual training plan and 
+              ensure its efficient implementation according to business 
+              objective and training priorities
+            </li>
+            <li>
+              Widely scans the environment to keep up and bring in the 
+              latest research, practices and technology
+            </li>
+          </ul>
+
+          <h4>Desired skills & competencies:</h4>
+          <ul>
+            <li>
+              Ability to set clear goals and timelines, manage 
+              communications between multiple stakeholders, 
+              communicate the need and benefit, inspire and convince
+            </li>
+            <li>Excellent analytical and influencing skills</li>
+            <li>
+              Strong conceptual thinking skills with an ability to 
+              problem solve, recommend solutions and intuitively prioritize 
+              business issues
+            </li>
+            <li>
+              Strong issue & opportunity identification and solid operational 
+              understanding
+            </li>
+            <li>
+              Excellent interpersonal skills
+            </li>
+            <li>Resilience</li>
+            <li>
+              Ability to work on multiple projects simultaneously, managing 
+              time and resources to ensure work is completed efficiently within 
+              established timeframes
+            </li>
+            <li>Strong people management and motivation skills</li>
+            <li>Very good English written and oral</li>
+          </ul>
+
+          <h4>Must have technical and professional qualifications: </h4>
+          <ul>
+            <li>
+              Experience in Learning & Development especially in project 
+              management of learning and development programs
+            </li>
+            <li>Contact center experience</li>
+            <li>Outstanding knowledge of learning theories and training methodologies</li>
+            <li>A minimum of 3 years experience in training and development management</li>
+          </ul>
+
+          <h4>Additional qualifications</h4>
+          <p>Healthcare related studies</p>
+          <p>
+            A “can do” attitude and teamwork mentality are a must. We 
+            celebrate a customer-centric mindset, unconventional ideas 
+            and approaches, and a culture of excellence and continuous 
+            improvement. If that sounds like something you’d like to be a 
+            part of, please consider joining our team!
+          </p>
+
+          <p>Please use the link below to apply:</p>
+          <a href="https://epikast.bamboohr.com/careers/39">Apply</a>
+        `,
+        file: 'https://epikast.bamboohr.com/careers/39',
+      },
+      {
+        title: 'Medical Representative',
+        location: 'Athens, Greece',
+        description: `
+          <p>
+            At Epikast we redefine how biopharmaceutical companies engage 
+            with their most important stakeholders: patients, caregivers 
+            and prescribers.
+          </p>
+          <p>
+            In your role as a Medical Representative, you will engage with 
+            sophisticated physicians and other healthcare professionals in 
+            the US. You will better educate them about the latest 
+            developments within a particular disease area, and/or articulate 
+            the benefits of a specific drug.
+          </p>
+
+          <h4>Desired skills & competencies:</h4>
+          <ul>
+            <li>Sales skills </li>
+            <li>Customer centric attitude</li>
+            <li>Relationship Building</li>
+            <li>Coordination skills, ability to navigate complex orgs</li>
+            <li>Attention to detail</li>
+            <li>Resilience</li>
+            <li>Knowledge of the eye care market</li>
+            <li>Expert command of English (C2 level)</li>
+          </ul>
+
+          <p>Required Academic background:</p>
+          <p> Experience in eyecare required (studies in optometry); 
+              a plus for degrees in biology (with some expertise in the eye), 
+              MD, ophthalmology specialization, etc.
+          </p>
+
+          <h4>What we offe</h4>
+          <ul>
+            <li>Flexible and friendly environment</li>
+            <li>Industry-leading compensation and benefits</li>
+            <li>Ample space for personal and professional development</li>
+          </ul>
+
+          <p> A “can do” attitude and teamwork mentality are a must. We 
+              celebrate a customer-centric mindset, unconventional ideas 
+              and approaches, and a culture of excellence and continuous 
+              improvement. If that sounds like something you’d like to be 
+              a part of, please consider joining our team!
+          </p>
+
+          <p>Please use the link below to apply:</p>
+          <a href="https://epikast.bamboohr.com/careers/40">Apply</a>
+        `,
+        file: 'https://epikast.bamboohr.com/careers/40',
+      },
+      {
+        title: 'Medical Representative Team Lead',
+        location: 'Athens, Greece',
+        description: `
+          <p>
+            At Epikast we redefine how biopharmaceutical companies engage 
+            with their most important stakeholders: patients, caregivers 
+            and prescribers.
+          </p>
+          <p>
+            In your role as a Medical Rep Team Lead you will engage with 
+            sophisticated physicians and other healthcare professionals in the US, 
+            while also motivating and leading a team that does the same. 
+            You will ensure that your team better educates these professionalsabout 
+            the latest developments within a particular disease area, and/or articulate 
+            the benefits of a specific drug.
+          </p>
+
+          <h4>Desired skills & competencies:</h4>
+          <ul>
+            <li>At least 1 year of experience in managing high performing sales teams</li>
+            <li>A minimum of 5 years in sales</li>
+            <li>Customer centric attitude</li>
+            <li>Relationship Building </li>
+            <li>Coordination skills, ability to navigate complex orgs</li>
+            <li>Attention to detail</li>
+            <li>Resilience</li>
+            <li>Knowledge of the eye care market</li>
+            <li>Expert command of English (C2 level)</li>
+          </ul>
+
+          <p>Required Academic background:</p>
+          <p> Experience in eyecare required (studies in optometry); 
+              a plus for degrees in biology (with some expertise in the eye), 
+              MD, ophthalmology specialization, etc.
+          </p>
+
+          <h4>What we offe</h4>
+          <ul>
+            <li>Flexible and friendly environment</li>
+            <li>Industry-leading compensation and benefits</li>
+            <li>Ample space for personal and professional development</li>
+          </ul>
+
+          <p> A “can do” attitude and teamwork mentality are a must. We 
+              celebrate a customer-centric mindset, unconventional ideas 
+              and approaches, and a culture of excellence and continuous 
+              improvement. If that sounds like something you’d like to be 
+              a part of, please consider joining our team!
+          </p>
+
+          <p>Please use the link below to apply:</p>
+          <a href="https://epikast.bamboohr.com/careers/42">Apply</a>
+        `,
+        file: 'https://epikast.bamboohr.com/careers/42',
+      },
+      {
         title: 'Case Manager (Patient Support) - English',
         location: 'Athens, Greece',
         description: `
